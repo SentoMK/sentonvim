e8f5b822d8a131585dd866527d8cd342794cca5d
SentoMK <sentomk040924@gmail.com>
2025-05-26 23:06:12 +0800

    update: comments.lua

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,17 +1,16 @@
 return {
-    {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup({
-                toggler = {
-                    line = '<leader>c',
-                    block = '<leader>bc',
-                },
-                opleader = {
-                    line = '<leader>c'
-                    block = '<leader>b',
-                }
-            })
-        end
-    }
+  'numToStr/Comment.nvim',
+  config = function()
+    require('Comment').setup({
+      toggler = {
+        line = '<leader>cc', -- 行注释切换
+        block = '<leader>bc', -- 块注释切换
+      },
+      opleader = {
+        line = '<leader>c',
+        block = '<leader>b',
+      }
+    })
+  end
 }
+
